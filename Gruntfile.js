5c8d19d4ba79d0c0cc7ee2fc418a3321d7cd40d7
momoko8443 <momoko8443@163.com>
2015-06-08 16:52:19 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,42 @@
-module.exports = function(grunt){
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        concat:{
-            options:{
-                stripBanners:true,
-                banner:'/*! <%= pkg.name %> - v<%= pkg.version%> - ' +
-                        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+        concat: {
+            options: {
+                stripBanners: true,
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version%> - ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %> */'
             },
-            dist:{
-                src:['ukulele/*/*.js'],
-                dest:'build/js/all.js'
+            dist: {
+                src: ['ukulele/*/*.js'],
+                dest: 'build/js/all.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'build/js/all.js',
+                dest: 'build/js/all.min.js'
+            }
+        },
+        jshint: {
+            // define the files to lint
+            files: ['ukulele/*/*.js'],
+            // configure JSHint (documented at http://www.jshint.com/docs/)
+            options: {
+                // more options here if you want to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
             }
         }
     });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.registerTask('default', ['concat']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['jshint','concat', 'uglify']);
 };
