10267693f4866cc1765db7e854bca7d4c3fc737f
momoko8443 <momoko8443@163.com>
2015-06-24 23:23:23 +0800

    test case

--- src/ukulele/util/UkuleleUtil.js
+++ src/ukulele/util/UkuleleUtil.js
@@ -1,9 +1,36 @@
 function UkuleleUtil() {
-    'use strict';
+	'use strict';
 }
 
-UkuleleUtil.getFinalAttribute = function (expression){
-    var temp = expression.split(".");
-    temp.shift();
-    return temp.join(".");
+UkuleleUtil.getFinalAttribute = function(expression) {
+	var temp = expression.split(".");
+	temp.shift();
+	return temp.join(".");
+};
+
+UkuleleUtil.isRepeat = function($element) {
+	if ($element.attr("uku-repeat")) {
+		return true;
+	}
+	return false;
+};
+
+UkuleleUtil.isInRepeat = function($element) {
+	var parents = $element.parents();
+	for (var i = 0; i < parents.length; i++) {
+		var parent = parents[i];
+		var b = $(parent).attr("uku-repeat");
+		if (b) {
+			return true;
+		}
+	}
+	return false;
+};
+
+UkuleleUtil.getBoundModelInstantName = function(expression) {
+	var controlInstName = expression.split('.')[0];
+	if (controlInstName) {
+		return controlInstName;
+	}
+	return null;
 };
--- test/util/UkuleleUtilSpec.js
+++ test/util/UkuleleUtilSpec.js
@@ -1,10 +1,33 @@
 describe("UkuleleUtil Test Suite", function() {
-    it("test getFinalAttribute", function() {
-        var expression1 = "myCtrl.name";
-        var attr = UkuleleUtil.getFinalAttribute(expression1);
-        var expression2 = "myCtrl.child.name";
-        var attr2 = UkuleleUtil.getFinalAttribute(expression2);
-        expect(attr).toBe("name");
-        expect(attr2).toBe("child.name");
-    });
-});
+	it("test getFinalAttribute", function() {
+		var expression1 = "myCtrl.name";
+		var attr = UkuleleUtil.getFinalAttribute(expression1);
+		var expression2 = "myCtrl.child.name";
+		var attr2 = UkuleleUtil.getFinalAttribute(expression2);
+		expect(attr).toBe("name");
+		expect(attr2).toBe("child.name");
+	});
+
+	it("test isRepeat", function() {
+		var element = '<button value="test">click</button';
+		expect(UkuleleUtil.isRepeat($(element))).toBe(false);
+		var element1 = '<button value="test" uku-repeat="item in myCtrl.items">click</button';
+		expect(UkuleleUtil.isRepeat($(element1))).toBe(true);
+	});
+
+	
+	it("test isInRepeat", function() {
+			var $html = $('<li uku-repeat="item in myCtrl.items"><input type="text"><li>');
+			var $element = $html.find("input:first");
+			expect(UkuleleUtil.isInRepeat($element)).toBe(true);
+		});
+	
+	it("test getBoundModelInstantName", function() {
+		var expression1 = "myCtrl.name";
+		var attr = UkuleleUtil.getBoundModelInstantName(expression1);
+		var expression2 = "myCtrl.child.name";
+		var attr2 = UkuleleUtil.getBoundModelInstantName(expression2);
+		expect(attr).toBe("myCtrl");
+		expect(attr2).toBe("myCtrl");
+	});
+}); 
