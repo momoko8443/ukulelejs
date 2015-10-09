350942d7a1c07b254a15e218a8c2e31cef4e263c
momoko8443 <momoko8443@163.com>
2015-10-09 13:59:28 +0800

    modified example_09.html

--- resources/js/example09.js
+++ resources/js/example09.js
@@ -1,10 +1,10 @@
 define("Example09Ctrl", function () {
     return function (uku) {
-        this.syncData = "will show json from sync request";
+        this.asyncData = "will show json from async request";
         var self = this;
-        this.callSync = function () {
+        this.callAsync = function () {
             $.get("resources/data/data.json", function (data, status) {
-                self.syncData = data.name;
+                self.asyncData = data.name;
                 uku.refresh("ex09Ctrl"); //you can use "uku.refresh(['myCtrl','myCtrl2'])" to refresh multiple controllers.
             });
         };
