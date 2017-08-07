d2f22c8fca462bf41b06ee3a72c8b254fd1fd850
Jake Zheng <momoko8443@163.com>
2017-08-07 23:15:38 +0800

    fix: <message>

--- src/core/ElementActionBinder.ts
+++ src/core/ElementActionBinder.ts
@@ -1,6 +1,6 @@
-import {UkuleleUtil} from '../util/UkuleleUtil';
-import {EventListener} from '../extend/EventListener';
-import {Selector} from '../extend/Selector';
+import { UkuleleUtil } from '../util/UkuleleUtil';
+import { EventListener } from '../extend/EventListener';
+import { Selector } from '../extend/Selector';
 
 function elementChangedBinder(element, tagName, controllerModel, handler, host) {
     let elementStrategies = [inputTextCase, textareaCase, selectCase, checkboxCase, radioCase];
@@ -19,14 +19,14 @@ function inputTextCase(element, tagName, controllerModel, handler, host) {
     if (elementName === "INPUT" && isSupportInputType(element) && tagName === "value") {
         let eventType = 'change';
         let inputType = element.getAttribute('type');
-        if(inputType === "text"){
+        if (inputType === "text") {
             eventType = 'input';
         }
-        EventListener.addEventListener(element,eventType,(e)=>{
+        EventListener.addEventListener(element, eventType, (e) => {
             let attr = element.getAttribute("uku-" + tagName);
-            UkuleleUtil.setFinalValue(controllerModel.controllerInstance,attr,element.value);
+            UkuleleUtil.setFinalValue(controllerModel.controllerInstance, attr, element.value);
             if (handler) {
-                handler.call(host,controllerModel.alias, element);
+                handler.call(host, controllerModel.alias, element);
             }
         });
         return true;
@@ -45,11 +45,11 @@ function isSupportInputType(element) {
 function textareaCase(element, tagName, controllerModel, handler, host) {
     let elementName = element.tagName;
     if (elementName === "TEXTAREA" && tagName === "value") {
-        EventListener.addEventListener(element,'input',function(e){
+        EventListener.addEventListener(element, 'input', function (e) {
             let attr = element.getAttribute("uku-" + tagName);
-            UkuleleUtil.setFinalValue(controllerModel.controllerInstance,attr,element.value);
+            UkuleleUtil.setFinalValue(controllerModel.controllerInstance, attr, element.value);
             if (handler) {
-                handler.call(host,controllerModel.alias, element);
+                handler.call(host, controllerModel.alias, element);
             }
         });
         return true;
@@ -60,23 +60,23 @@ function textareaCase(element, tagName, controllerModel, handler, host) {
 function selectCase(element, tagName, controllerModel, handler, host) {
     let elementName = element.tagName;
     if ((elementName === "SELECT" && tagName === "selected")) {
-        EventListener.addEventListener(element,'change',function(e){
+        EventListener.addEventListener(element, 'change', function (e) {
             let attr = element.getAttribute("uku-" + tagName);
             let key;
             let tmpArr = attr.split("|");
             attr = tmpArr[0];
 
-            let options = Selector.querySelectorAll(element,"option");
+            let options = Selector.querySelectorAll(element, "option");
             for (let j = 0; j < options.length; j++) {
-                let option:HTMLOptionElement = options[j] as HTMLOptionElement;
+                let option: HTMLOptionElement = options[j] as HTMLOptionElement;
                 if (option.selected) {
                     let selectedItem = JSON.parse(option.getAttribute("data-item"));
-                    UkuleleUtil.setFinalValue(controllerModel.controllerInstance,attr,selectedItem);
-                    
+                    UkuleleUtil.setFinalValue(controllerModel.controllerInstance, attr, selectedItem);
+
                 }
             }
             if (handler) {
-                handler.call(host,controllerModel.alias, element);
+                handler.call(host, controllerModel.alias, element);
             }
         });
         return true;
@@ -88,12 +88,12 @@ function checkboxCase(element, tagName, controllerModel, handler, host) {
     let elementName = element.tagName;
 
     if (elementName === "INPUT" && tagName === "value" && element.getAttribute("type") === "checkbox") {
-        EventListener.addEventListener(element,'change',function(e){
+        EventListener.addEventListener(element, 'change', function (e) {
             let attr = element.getAttribute("uku-" + tagName);
-            
-            UkuleleUtil.setFinalValue(controllerModel.controllerInstance,attr,element.checked);
+
+            UkuleleUtil.setFinalValue(controllerModel.controllerInstance, attr, element.checked);
             if (handler) {
-                handler.call(host,controllerModel.alias, element);
+                handler.call(host, controllerModel.alias, element);
             }
         });
         return true;
@@ -105,13 +105,13 @@ function radioCase(element, tagName, controllerModel, handler, host) {
     let elementName = element.tagName;
 
     if (elementName === "INPUT" && tagName === "selected" && element.getAttribute("type") === "radio") {
-        EventListener.addEventListener(element,'change',function(e){
+        EventListener.addEventListener(element, 'change', function (e) {
             let attr = element.getAttribute("uku-" + tagName);
-            
+
             if (element.checked) {
-                UkuleleUtil.setFinalValue(controllerModel.controllerInstance,attr,element.value);
+                UkuleleUtil.setFinalValue(controllerModel.controllerInstance, attr, element.value);
                 if (handler) {
-                    handler.call(host,controllerModel.alias, element);
+                    handler.call(host, controllerModel.alias, element);
                 }
             }
         });
@@ -120,4 +120,4 @@ function radioCase(element, tagName, controllerModel, handler, host) {
     return false;
 }
 
-export {elementChangedBinder};
+export { elementChangedBinder };
