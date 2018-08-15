6f3375993daa80e2b198e750e66413df2d112d52
Jake.Zheng <Huibin.zheng@hpe.com>
2018-08-15 20:28:33 +0800

    fix: <remove addEventlistener with jquery>

--- src/extend/EventListener.ts
+++ src/extend/EventListener.ts
@@ -1,11 +1,5 @@
 export class EventListener{
     static addEventListener(element:HTMLElement,eventType:string,handler:any):void{
-        if(window.hasOwnProperty('jQuery') && typeof window['jQuery'] !== undefined){
-            return window['jQuery'](element).on(eventType,(e)=>{
-                handler && handler(e);
-            });
-        }else{
-            return element.addEventListener(eventType,handler);
-        }
+        return element.addEventListener(eventType,handler);
     }
 }
